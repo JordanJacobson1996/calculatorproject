c67fdd532bccdb3d4dae66d5570e5b51f0513a0e
Jordan Jacobson <jordandurjacobson@gmail.com>
2022-04-24 14:55:53 +1000

    add event listener for pressing the buttons and returning their id

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+const buttons = document.querySelectorAll('button');
+
 function add (num1, num2){
     return num1 + num2;
 }
@@ -13,3 +15,9 @@ function multiply (num1, num2){
 function divide (num1, num2){
     return num1/num2;
 }
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () =>{
+        console.log(button.id);
+    });
+});
